ee56d6244b1b4fcdefb14bccefbac8410cd77a4d
Prasek2024 <163199807+Prasek2024@users.noreply.github.com>
2024-03-26 19:04:35 +0100

    zmeny

--- Lekce9/file opening.py
+++ Lekce9/file opening.py
@@ -1,17 +1,14 @@
-
-
-file = open('test.txt', 'w')
+file = open('../test.txt', 'w')
 file.write("test text")
 file.close()
 
 #pri cteni musi soubor existovat
-file = open('test.txt', 'r')
+file = open('../test.txt', 'r')
 data = file.read()
 file.close()
 print(data)
 
-file = open('test.txt', 'a')
+file = open('../test.txt', 'a')
 file.write("/n druhy radek")
 file.close()
 print(data)
-                              
